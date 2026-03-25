9883317bdc48a50709abfbc2cf03bede06c1919d
medahlia <matvienko.dash.0k@gmail.com>
2026-03-25 22:03:41 +0200

    add SequentialMethod + tests

--- lab 3/TPO_lab3/src/SequentialMethod.java
+++ lab 3/TPO_lab3/src/SequentialMethod.java
@@ -1,2 +1,27 @@
 public class SequentialMethod {
+
+    public static Matrix multiply(Matrix a, Matrix b) {
+
+        if (a.getCols() != b.getRows()) {
+            throw new IllegalArgumentException("Matrices cannot be multiplied");
+        }
+
+        int rows = a.getRows();
+        int cols = b.getCols();
+        int common = a.getCols();
+
+        Matrix result = new Matrix(rows, cols);
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                int sum = 0;
+                for (int k = 0; k < common; k++) {
+                    sum += a.getValue(i, k) * b.getValue(k, j);
+                }
+                result.setValue(i, j, sum);
+            }
+        }
+
+        return result;
+    }
 }
--- lab 3/TPO_lab3/src/Tests/MatrixTest.java
+++ lab 3/TPO_lab3/src/Tests/MatrixTest.java
@@ -53,4 +53,56 @@ class MatrixTest {
         // Можемо перевірити, що обʼєкти різні
         assertNotSame(m1, m2, "Кожен виклик має створювати новий обʼєкт матриці");
     }
+
+    // перевірка на коректність обчислення послідовного методу
+    @Test
+    void multiply_shouldReturnCorrectResult() {
+
+        Matrix m1 = new Matrix(2, 2);
+        m1.setValue(0, 0, 1);
+        m1.setValue(0, 1, 2);
+        m1.setValue(1, 0, 3);
+        m1.setValue(1, 1, 4);
+
+        Matrix m2 = new Matrix(2, 2);
+        m2.setValue(0, 0, 5);
+        m2.setValue(0, 1, 6);
+        m2.setValue(1, 0, 7);
+        m2.setValue(1, 1, 8);
+
+        Matrix result = SequentialMethod.multiply(m1, m2);
+
+        assertEquals(19, result.getValue(0,0));
+        assertEquals(22, result.getValue(0,1));
+        assertEquals(43, result.getValue(1,0));
+        assertEquals(50, result.getValue(1,1));
+    }
+
+    // перевірка на коректність обчислення послідовного методу
+    @Test
+    void multiply_differentSizeMatrices_shouldReturnCorrectResult() {
+
+        Matrix m1 = new Matrix(2, 3);
+        m1.setValue(0, 0, 1);
+        m1.setValue(0, 1, 2);
+        m1.setValue(0, 2, 3);
+        m1.setValue(1, 0, 4);
+        m1.setValue(1, 1, 5);
+        m1.setValue(1, 2, 6);
+
+        Matrix m2 = new Matrix(3, 2);
+        m2.setValue(0, 0, 7);
+        m2.setValue(0, 1, 8);
+        m2.setValue(1, 0, 9);
+        m2.setValue(1, 1, 10);
+        m2.setValue(2, 0, 11);
+        m2.setValue(2, 1, 12);
+
+        Matrix result = SequentialMethod.multiply(m1, m2);
+
+        assertEquals(58, result.getValue(0,0));
+        assertEquals(64, result.getValue(0,1));
+        assertEquals(139, result.getValue(1,0));
+        assertEquals(154, result.getValue(1,1));
+    }
 }
