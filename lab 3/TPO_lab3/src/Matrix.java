df839006ff7d3bead32a7a58ed5a988c86ee3da9
medahlia <matvienko.dash.0k@gmail.com>
2026-03-25 14:07:30 +0200

    add structure

---
+++ lab 3/TPO_lab3/src/Matrix.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+
+public class Matrix {
+
+    private int[][] matrix;
+
+    public Matrix(int rows, int cols) {
+        matrix = new int[rows][cols];
+    }
+
+    // генерація випадкової матриці
+    public static Matrix generateRandom(int rows, int cols) {
+        Matrix matrix = new Matrix(rows, cols);
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                matrix.setValue(i, j, (int) (Math.random() * 10));
+        return matrix;
+    }
+
+    public int getValue(int row, int col) {
+        return matrix[row][col];
+    }
+
+    public void setValue(int row, int col, int value) {
+        matrix[row][col] = value;
+    }
+
+    public int getRows() {
+        return matrix.length;
+    }
+
+    public int getCols() {
+        return matrix[0].length;
+    }
+
+    public void print() {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ lab 3/TPO_lab3/src/StripedMethod.java
@@ -0,0 +1,2 @@
+public class StripedMethod {
+}
